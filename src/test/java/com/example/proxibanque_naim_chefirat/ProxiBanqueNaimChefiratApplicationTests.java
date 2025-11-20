47874b06d4fee5b9f0d1882697b301b957421722
Naim <naimchef@proton.me>
2025-11-20 14:22:57 +0100

    init repo

---
+++ src/test/java/com/example/proxibanque_naim_chefirat/ProxiBanqueNaimChefiratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxibanque_naim_chefirat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueNaimChefiratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
